e1face6793f0042f7ccec727d3240286f8f37a12
lin9587 <lin9587@aliyun.com>
2023-02-26 15:50:39 +0800

    show

--- table/src/Table.tsx
+++ table/src/Table.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 const Table: React.FC = ({}) => {
   return (
     <table border={1} summary="Monthly savings for the Flintstones family">
-      <tr>
-        <th>Month</th>
-        <th>Savings</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>Month</th>
+          <th>Savings</th>
+        </tr>
+      </thead>
       <tr>
         <td>January</td>
         <td>$100</td>
